7c08837d147155992c21c00bffc15b8779121d18
MarcosGerminiodeSouza <marcos.germiniodesouza@gmail.com>
2022-11-02 13:34:30 -0300

    JavaScript

---
+++ funcoes/parametros.js
@@ -0,0 +1,9 @@
+// pasando parametros.
+function incrementarJuros(valor, percentualJuros){
+    const valorDeAcrecimo = (percentualJuros / 100) * valor;
+    return valor + valorDeAcrecimo;
+}
+
+console.log(incrementarJuros(100, 10));
+console.log(incrementarJuros(100, 15));
+console.log(incrementarJuros(100, 20));
