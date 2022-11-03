35f91eb38c0ec0ef098fe7a359781ffc93f0f269
MarcosGerminiodeSouza <marcos.germiniodesouza@gmail.com>
2022-11-03 16:43:21 -0300

    constructor

--- objetos e classes/classes.js
+++ objetos e classes/classes.js
@@ -1,3 +1,4 @@
+/*
 class Pessoa {
     nome;
     idade;
@@ -21,3 +22,32 @@ marcos.idade = 26;
 vitor.descrever();
 renan.descrever();
 marcos.descrever();
+*/
+class Pessoa {
+    nome;
+    idade;
+    nascido;
+
+    // constructor é o que acontece quando uma Pessoa é instaciada.
+    constructor(nome, idade){
+        this.nome = nome;
+        this.idade = idade;
+        this.nascido = 2022 - idade;
+    }
+
+    descrever(){
+        console.log(`Meu nome é ${this.nome} e minha idade ${this.idade} anos.`);
+    }
+}
+
+const vitor = new Pessoa('Vitor Gerra', 25);
+const renan = new Pessoa('Renan de Paula', 30);
+const marcos = new Pessoa('Marcos Germinio', 26);
+/*
+vitor.descrever();
+renan.descrever();
+marcos.descrever();
+*/
+console.log(vitor);
+console.log(renan);
+console.log(marcos);
