1d5bfdf51fb95a440d45ee6bf34b599d3781b703
MarcosGerminiodeSouza <marcos.germiniodesouza@gmail.com>
2022-11-05 16:27:59 -0300

    every

---
+++ array/every.js
@@ -0,0 +1,15 @@
+/* //false, porque nem todos sao maior que 10
+const agenda = [12, 5, 8, 130, 44]
+
+console.log( agenda.every( elem => elem > 10 ) );
+*/
+/*
+const site = [
+    {id: 12, nome: 'Enzo', idade: 8},
+    {id: 47, nome: 'Emilly', idade: 17},
+    {id: 77, nome: 'Suely', idade: 48},
+    {id: 85, nome: 'Geraldo', idade: 52}
+]
+
+console.log( site.every( p => p >= 18 ) );
+*/
