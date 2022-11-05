4e5563b0943717f4d614d20da960b0a7b3501501
MarcosGerminiodeSouza <marcos.germiniodesouza@gmail.com>
2022-11-05 13:20:23 -0300

    map

---
+++ array/map.js
@@ -0,0 +1,41 @@
+/*
+const numeros = [1, 4, 5, 9, 14, 23];
+
+const multiplicador = numeros.map( function( elem ){
+    return elem * 2;
+} )
+console.log(multiplicador);
+*/
+/*
+const numeros = [ 1, 4, 5, 9, 14, 23 ]; // escrita moderna
+
+function multiplicar(num){
+    return num * 2;
+}
+
+const multiplicador = numeros.map( multiplicar );
+
+console.log(multiplicador);
+*/
+/*
+const fahrenheit = [0, 32, 45, 50, 75, 120];
+
+const celsius = fahrenheit.map( function(elem){
+    return Math.round((elem - 32) * 5/9 );
+} );
+console.log(celsius);
+*/
+/*
+const produtos = [
+    {
+        nome: "notebook",
+        preco: 2100
+    },
+    {
+        nome:"smartphone",
+        preco: 400
+    }
+]
+const precosEmReais = produtos.map(p => p.preco * 4)
+console.log(precosEmReais)
+*/
