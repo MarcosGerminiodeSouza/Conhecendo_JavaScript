f3fafb1abd9c9f212943de754b3dd1b11c78ffdb
MarcosGerminiodeSouza <marcos.germiniodesouza@gmail.com>
2022-11-05 13:33:03 -0300

    reduce

---
+++ array/reduce.js
@@ -0,0 +1,39 @@
+/*
+const produtos = [
+    {
+        nome: "notebook",
+        preco: 2100
+    },
+    {
+        nome:"smartphone",
+        preco: 400
+    },
+    {
+        nome: "carregador",
+        preco: 50
+    }
+]
+const total = produtos.reduce((a, b) => a + b.preco, 0)
+
+console.log(total)
+*/
+/*
+const produtos = [
+    {
+        nome: "notebook",
+        preco: 2100
+    },
+    {
+        nome:"smartphone",
+        preco: 400
+    },
+    {
+        nome: "carregador",
+        preco: 50
+    }
+]
+const total = produtos.map(p => p.preco * 4).reduce((a, b) => a + b, 0)
+
+console.log(total)
+*/
+
